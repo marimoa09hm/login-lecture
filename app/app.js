3fe07fdaedcf114cdb2ebf682d9872657a6e27c4
marimoa0924 <sky_yoon1@naver.com>
2023-05-29 04:24:49 +0900

    값 불러오기

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const app = express();
 
-app.set("views", "./src/views")
-app.set("view engine", "ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
-const home = require("./src/routes/home")
+const home = require("./src/routes/home");
 app.use("/", home);
 
 module.exports = app;
